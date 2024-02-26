7143f923f753d3644105d47c726fdcd9197aedf4
meyyyy_art <142365545+meydianaekaputri@users.noreply.github.com>
2024-02-26 16:06:28 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="row p-3 bg-ptimary text-white">
+    <div class="col-lg-1">
+      <img src="~/assets/img/LOGO-SMK4.png" alt="logo SMKN 4">
+    </div>
+
+    <div class="col-lg-11">
+      <h1>Perpus Digital</h1>
+      <address>
+      SMKN 4 Tasikmalaya
+      Jl.Depok, Sukamenak, Purbaratu
+    </address>
+   </div>
+  </div>
+</template>
+
---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="container-fluid">
+     <header />
+     <slot />
+  </div>
+</template>
